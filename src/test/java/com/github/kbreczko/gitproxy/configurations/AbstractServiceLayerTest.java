b1c93d55c8c53ddafca5deb6ee81e27ed9525606
kbreczko <kamil.breczko@gmail.com>
2021-06-29 21:14:44 +0200

    [#3] add integration test

--- src/test/java/com/github/kbreczko/gitproxy/configurations/AbstractServiceLayerTest.java
+++ src/test/java/com/github/kbreczko/gitproxy/configurations/AbstractServiceLayerTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {ServiceLayerTestConfiguration.class})
 @Sql(statements = "DELETE FROM user", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
